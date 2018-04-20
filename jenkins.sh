5e958653486bcfd8bf969c536e5d8ff7c4fb4151
Anshul Sirur <anshul.sirur@digital.cabinet-office.gov.uk>
2018-04-20 16:50:52 +0100

    BAU: Update publish.gradle to only publish hub-saml projects
    Author: @vixus0

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e # abort if you encounter an error
 set -o pipefail # abort if there is an error in a piped operation
-./gradlew -Pversion=$BUILD_NUMBER clean test intTest copyToLib
+./gradlew -Pversion=$BUILD_NUMBER clean test intTest copyToLib publish
 bin/build_and_upload_debs.rb
 ./gradlew outputDependencies -q > dependencies.properties
