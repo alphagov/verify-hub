d71cba1c08db6218f50ec8c426ab76b30a77affd
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-06-15 11:33:31 +0100

    bau: Publish hub-saml to bintray
    I thought the hub-saml jenkins job would do this, but it looks like that
    job is actually a vestige of before we moved hub-saml into hub and
    archived the repo.
    
    verify-hub's jenkins job isn't JJB'd, so I'll set up the relevant bits
    in the jenkins UI.

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e # abort if you encounter an error
 set -o pipefail # abort if there is an error in a piped operation
-./gradlew -Pversion=$BUILD_NUMBER clean test intTest copyToLib publish
+./gradlew -Pversion=$BUILD_NUMBER clean test intTest copyToLib publish bintrayUpload
 bin/build_and_upload_debs.rb
 ./gradlew outputDependencies -q > dependencies.properties
