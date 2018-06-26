7109a388a896eb8c3aa8fbf303a490b05c8b4b2c
stevebutler <steve.butler@digital.cabinet-office.gov.uk>
2018-06-26 14:01:41 +0100

    VCU-80: Enable dynamic SharedSecrets class loading
    In order for the hub to run in both a Java 8 and Java 10 JDK we need to dynamically select the correct classpath as it was moved between the two versions.
    
    This can be reverted and removed once we are fully building and running with Java 10 (or 9+)
    
    Co-authored-by: Alan Carter <alan.carter@digital.cabinet-office.gov.uk>, dbes-gds <38218115+dbes-gds@users.noreply.github.com>

---
+++ hub/saml-engine/src/main/java/uk/gov/ida/hub/samlengine/security/SharedSecretsWrapper.java
@@ -0,0 +1,28 @@
+package uk.gov.ida.hub.samlengine.security;
+
+import uk.gov.ida.common.shared.security.exceptions.KeyLoadingException;
+
+import java.io.FileDescriptor;
+
+public class SharedSecretsWrapper {
+    public static void setJavaIOFileDescriptorAccess(FileDescriptor fileDescriptor, int fileDescriptorNumber) {
+
+        /**
+         * Temporary code to dynamically load a moved class in a Java 10 JVM having been built with an 8 JDK
+         * To be removed when built with a 10 JDK
+         */
+        try {
+            String javaVersion = System.getProperty("java.specification.version");
+            String sharedSecretsClassPath = javaVersion.startsWith("1.8") ? "sun.misc.SharedSecrets" : "jdk.internal.misc.SharedSecrets";
+
+            Object javaIOFileDescriptorAccess = Class.forName(sharedSecretsClassPath).getMethod(
+                    "getJavaIOFileDescriptorAccess").invoke(null);
+
+            Class.forName("sun.misc.JavaIOFileDescriptorAccess").getMethod(
+                    "set", FileDescriptor.class, Integer.class).invoke(javaIOFileDescriptorAccess, fileDescriptor, fileDescriptorNumber);
+
+        } catch (Exception e) {
+            throw new KeyLoadingException(fileDescriptorNumber, e);
+        }
+    }
+}
