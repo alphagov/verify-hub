d50d989da09a7ab4320cc4c78e9af541cf8ce899
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2018-08-28 13:05:31 +0100

    Revert "No more saml engine hub certs"

--- hub/saml-engine/src/main/java/uk/gov/ida/hub/samlengine/exceptions/KeyLoadingException.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.ida.hub.samlengine.exceptions;
-
-public class KeyLoadingException extends RuntimeException {
-    public KeyLoadingException(Throwable t) {
-        super(t);
-    }
-}
