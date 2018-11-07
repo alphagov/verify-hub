aa0b5930720d600c4a31320ff522035f3873d091
Tom Rosier <tom.rosier@digital.cabinet-office.gov.uk>
2018-11-07 13:36:28 +0000

    EID-1053: Fixing bug in eIDAS journey when user presses back after authentication fails.
    Added in the handling of a EidasAuthnFailedErrorState to Countries Service,
    which should trigger going back to a session started state when landing on the country selection page.

--- hub/policy/src/main/java/uk/gov/ida/hub/policy/domain/controller/EidasAuthnFailedErrorStateController.java
+++ hub/policy/src/main/java/uk/gov/ida/hub/policy/domain/controller/EidasAuthnFailedErrorStateController.java
@@ -3,10 +3,11 @@ package uk.gov.ida.hub.policy.domain.controller;
 import uk.gov.ida.hub.policy.domain.ResponseFromHubFactory;
 import uk.gov.ida.hub.policy.domain.StateTransitionAction;
 import uk.gov.ida.hub.policy.domain.state.EidasAuthnFailedErrorState;
+import uk.gov.ida.hub.policy.domain.state.EidasCountrySelectedState;
 import uk.gov.ida.hub.policy.domain.state.SessionStartedState;
 import uk.gov.ida.hub.policy.logging.HubEventLogger;
 
-public class EidasAuthnFailedErrorStateController extends AbstractAuthnFailedErrorStateController<EidasAuthnFailedErrorState> implements RestartJourneyStateController {
+public class EidasAuthnFailedErrorStateController extends AbstractAuthnFailedErrorStateController<EidasAuthnFailedErrorState> implements RestartJourneyStateController, EidasCountrySelectingStateController {
 
     public EidasAuthnFailedErrorStateController(
             EidasAuthnFailedErrorState state,
@@ -22,5 +23,24 @@ public class EidasAuthnFailedErrorStateController extends AbstractAuthnFailedErr
         final SessionStartedState sessionStartedState = createSessionStartedState();
         hubEventLogger.logSessionMovedToStartStateEvent(sessionStartedState);
         stateTransitionAction.transitionTo(sessionStartedState);
+    }
+
+    @Override
+    public void selectCountry(final String countryEntityId) {
+        EidasCountrySelectedState countrySelectedState = new EidasCountrySelectedState(
+                countryEntityId,
+                state.getRelayState(),
+                state.getRequestId(),
+                state.getRequestIssuerEntityId(),
+                state.getSessionExpiryTimestamp(),
+                state.getAssertionConsumerServiceUri(),
+                state.getSessionId(),
+                state.getTransactionSupportsEidas(),
+                state.getLevelsOfAssurance(),
+                state.getForceAuthentication().orNull()
+        );
+
+        stateTransitionAction.transitionTo(countrySelectedState);
+        hubEventLogger.logCountrySelectedEvent(countrySelectedState);
     }
 }
