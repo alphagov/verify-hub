e49e6684e0bb4c613b89a6ce05f4a43b5c770601
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2020-03-18 14:56:40 +0000

    HUB-520: crash config service unable to load cert
     - move error message into NoCertificateFoundException

--- hub/config/src/main/java/uk/gov/ida/hub/config/exceptions/NoCertificateFoundException.java
+++ hub/config/src/main/java/uk/gov/ida/hub/config/exceptions/NoCertificateFoundException.java
@@ -1,4 +1,6 @@
 package uk.gov.ida.hub.config.exceptions;
 
 public class NoCertificateFoundException extends RuntimeException {
+    public NoCertificateFoundException(){}
+    public NoCertificateFoundException(String message) { super(message); }
 }
