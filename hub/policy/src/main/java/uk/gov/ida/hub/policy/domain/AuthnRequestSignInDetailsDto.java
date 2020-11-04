747c2d8a2b3f164968fa73a6e8da60c81f60c64b
Phil Miller <phillip.miller@digital.cabinet-office.gov.uk>
2020-11-04 08:39:41 +0000

    spike

--- hub/policy/src/main/java/uk/gov/ida/hub/policy/domain/AuthnRequestSignInDetailsDto.java
+++ hub/policy/src/main/java/uk/gov/ida/hub/policy/domain/AuthnRequestSignInDetailsDto.java
@@ -1,9 +1,15 @@
 package uk.gov.ida.hub.policy.domain;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 public class AuthnRequestSignInDetailsDto {
 
     private String requestIssuerId;
     private boolean transactionSupportsEidas;
+    private boolean beforeEUExit;
 
     @SuppressWarnings("unused")//Needed by JAXB
     private AuthnRequestSignInDetailsDto() { }
@@ -14,6 +20,7 @@ public class AuthnRequestSignInDetailsDto {
 
         this.requestIssuerId = requestIssuerId;
         this.transactionSupportsEidas = transactionSupportsEidas;
+        this.beforeEUExit = evaluateBeforeEUExit();
     }
 
     public String getRequestIssuerId() {
@@ -22,5 +29,15 @@ public class AuthnRequestSignInDetailsDto {
 
     public boolean getTransactionSupportsEidas() {
         return transactionSupportsEidas;
+    }
+
+    public boolean getBeforeEUExit() {
+        return beforeEUExit;
+    }
+
+    public boolean evaluateBeforeEUExit() {
+        LocalDateTime euExit = LocalDateTime.of(2020, Month.OCTOBER, 28, 23, 59, 59);
+        ZonedDateTime exitDateTime = ZonedDateTime.of(euExit, ZoneId.systemDefault());
+        return ZonedDateTime.now(ZoneId.systemDefault()).isBefore(exitDateTime);
     }
 }
