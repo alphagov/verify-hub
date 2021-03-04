ed8d03066b3e0c9a58f1e93d8fa07f99c1a380f1
Phil Miller <phillip.miller@digital.cabinet-office.gov.uk>
2021-03-04 13:59:28 +0000

    Remove eIDAS, Country and Countries java sources

--- hub-saml/src/main/java/uk/gov/ida/saml/hub/transformers/inbound/CountryAuthenticationStatusUnmarshaller.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.ida.saml.hub.transformers.inbound;
-
-import uk.gov.ida.saml.hub.domain.CountryAuthenticationStatus;
-
-public class CountryAuthenticationStatusUnmarshaller extends AuthenticationStatusUnmarshallerBase<CountryAuthenticationStatus.Status, CountryAuthenticationStatus> {
-    public CountryAuthenticationStatusUnmarshaller() {
-        super(new SamlStatusToCountryAuthenticationStatusCodeMapper(), new CountryAuthenticationStatus.CountryAuthenticationStatusFactory());
-    }
-}
--- hub/policy/src/main/java/uk/gov/ida/hub/policy/domain/state/EidasCountrySelectingState.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.ida.hub.policy.domain.state;
-
-import uk.gov.ida.hub.policy.domain.State;
-
-import java.util.Optional;
-
-public interface EidasCountrySelectingState extends State {
-    Optional<String> getRelayState();
-}
