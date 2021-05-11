a0b57d61485ca3105be7f4de6cfceb46ba46b302
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2021-05-11 09:12:45 +0100

    Make the config integration tests work
    Updates the tests to run with junit 5 and make use of the junit 5
    extension that comes with dropwizard-guicey to set up our tests apps.
    Extends the provided class to build them with our own config.

--- hub/config/src/integration-test/java/uk/gov/ida/integrationtest/hub/config/apprule/MatchingServiceResourceIntegrationTest.java
+++ hub/config/src/integration-test/java/uk/gov/ida/integrationtest/hub/config/apprule/MatchingServiceResourceIntegrationTest.java
@@ -1,20 +1,21 @@
 package uk.gov.ida.integrationtest.hub.config.apprule;
 
-import helpers.JerseyClientConfigurationBuilder;
-import io.dropwizard.client.JerseyClientBuilder;
-import io.dropwizard.client.JerseyClientConfiguration;
 import io.dropwizard.jersey.errors.ErrorMessage;
-import io.dropwizard.util.Duration;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Test;
+import io.dropwizard.testing.ResourceHelpers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import ru.vyarus.dropwizard.guice.test.ClientSupport;
+import ru.vyarus.dropwizard.guice.test.jupiter.ext.TestDropwizardAppExtension;
+import uk.gov.ida.hub.config.ConfigApplication;
 import uk.gov.ida.hub.config.Urls;
 import uk.gov.ida.hub.config.dto.MatchingServiceConfigDto;
-import uk.gov.ida.integrationtest.hub.config.apprule.support.ConfigAppRule;
+import uk.gov.ida.integrationtest.hub.config.apprule.support.ConfigAppExtension;
 import uk.gov.ida.shared.utils.string.StringEncoding;
 
-import javax.ws.rs.client.Client;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
 import java.util.Collection;
 
@@ -25,13 +26,12 @@ import static uk.gov.ida.hub.config.domain.builders.MatchingServiceConfigBuilder
 import static uk.gov.ida.hub.config.domain.builders.IdentityProviderConfigDataBuilder.anIdentityProviderConfigData;
 
 public class MatchingServiceResourceIntegrationTest {
-
-    public static Client client;
+    public static ClientSupport client;
     private static final String ENTITY_ID = "test-ms-entity-id";
     private static final String MATCHING_URI = "http://foo.bar/matching-service-uri";
 
-    @ClassRule
-    public static ConfigAppRule configAppRule = new ConfigAppRule()
+    @RegisterExtension
+    static TestDropwizardAppExtension app = ConfigAppExtension.forApp(ConfigApplication.class)
             .addTransaction(aTransactionConfigData()
                     .withEntityId("rp-entity-id")
                     .withMatchingServiceEntityId(ENTITY_ID)
@@ -47,41 +47,46 @@ public class MatchingServiceResourceIntegrationTest {
             .addIdp(anIdentityProviderConfigData()
                     .withEntityId("idp-entity-id")
                     .withOnboarding(singletonList("rp-entity-id"))
-                    .build());
+                    .build())
+            .writeFederationConfig()
+            .withDefaultConfigOverridesAnd()
+            .withClearedCollectorRegistry()
+            .config(ResourceHelpers.resourceFilePath("config.yml"))
+            .randomPorts()
+            .create();
 
-
-    @BeforeClass
-    public static void setUp() throws Exception {
-        configAppRule.newApplication();
-        JerseyClientConfiguration jerseyClientConfiguration = JerseyClientConfigurationBuilder.aJerseyClientConfiguration().withTimeout(Duration.seconds(10)).build();
-        client = new JerseyClientBuilder(configAppRule.getEnvironment()).using(jerseyClientConfiguration).build(MatchingServiceResourceIntegrationTest.class.getSimpleName());
+    @BeforeAll
+    static void setup(ClientSupport clientSupport) {
+        client = clientSupport;
     }
 
     @Test
     public void getMatchingServices_returnsOkAndListOfMatchingServices(){
-        Response response = client.target(configAppRule.getUri(Urls.ConfigUrls.MATCHING_SERVICE_ROOT).build()).request().get();
+        Response response = client.targetMain(Urls.ConfigUrls.MATCHING_SERVICE_ROOT).request().buildGet().invoke();
         assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
         assertThat(response.readEntity(Collection.class).size()).isEqualTo(1);
     }
 
     @Test
     public void getMatchingService_returnsMatchingServiceForEntityId(){
-        String entityId = ENTITY_ID;
-        URI uri = configAppRule.getUri(Urls.ConfigUrls.MATCHING_SERVICE_RESOURCE).buildFromEncoded(StringEncoding.urlEncode(entityId).replace("+", "%20"));
+        Response response = getForEntityIdAndPath(ENTITY_ID, Urls.ConfigUrls.MATCHING_SERVICE_RESOURCE);
 
-        Response response = client.target(uri.toASCIIString()).request().get();
         assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
         MatchingServiceConfigDto ms = response.readEntity(MatchingServiceConfigDto.class);
-        assertThat(ms.getEntityId()).isEqualTo(entityId);
+        assertThat(ms.getEntityId()).isEqualTo(ENTITY_ID);
         assertThat(ms.getUri().toString()).isEqualTo(MATCHING_URI);
     }
 
     @Test
     public void getMatchingService_returnsInternalServerError(){
-        String entityId = "not-found";
-        URI uri = configAppRule.getUri(Urls.ConfigUrls.MATCHING_SERVICE_RESOURCE).buildFromEncoded(StringEncoding.urlEncode(entityId).replace("+", "%20"));
-        Response response = client.target(uri.toASCIIString()).request().get();
+        Response response = getForEntityIdAndPath("not-found", Urls.ConfigUrls.MATCHING_SERVICE_RESOURCE);
+
         assertThat(response.getStatus()).isEqualTo(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
         assertThat(response.readEntity(ErrorMessage.class).getMessage()).contains("There was an error processing your request.");
+    }
+
+    private Response getForEntityIdAndPath(String entityId, String path) {
+        URI uri = UriBuilder.fromPath(path).buildFromEncoded(StringEncoding.urlEncode(entityId).replace("+", "%20"));
+        return client.targetMain(uri.toString()).request().buildGet().invoke();
     }
 }
